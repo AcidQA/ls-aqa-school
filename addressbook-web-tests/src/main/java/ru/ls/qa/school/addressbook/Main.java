3aa44a631ea818522cdd5290c6dc5b7545289fe4
abshpak <anton.shpak@ligastavok.ru>
2024-09-26 20:23:16 +0300

    Update_Main_4

--- addressbook-web-tests/src/main/java/ru/ls/qa/school/addressbook/Main.java
+++ addressbook-web-tests/src/main/java/ru/ls/qa/school/addressbook/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("А вот и я!");
+        System.out.println("Всем пиздец!");
     }
 }
